0f51f8bf3f796b417f10d04231f9f03519c6e595
CharlesKasasira <charleskasasira01@gmail.com>
2021-07-15 11:43:47 +0300

    Integration testing

---
+++ session6/integration.js
@@ -0,0 +1,14 @@
+let firstName, fullName
+
+firstName = (name = '') => { 
+    return name 
+}
+fullName = (first_name = '', last_name = '') => {
+    return first_name + ' ' + last_name 
+}
+
+
+module.exports = { 
+    firstName, 
+    fullName 
+};
