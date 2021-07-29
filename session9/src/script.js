bbf24b86314ef110e373f6803beef32cb051f1a7
CharlesKasasira <charleskasasira01@gmail.com>
2021-07-29 12:55:44 +0300

    explanations

--- session9/src/script.js
+++ session9/src/script.js
@@ -2,21 +2,26 @@ const TABLE_DATA = document.getElementById('table-data')
 
 let generatedHTMLRows = ''
 
+
+/**
+ * Read this resource https://developer.mozilla.org/en-US/docs/Web/APT/Fetch_API/Using_fetch
+ */
+// Explain the fetch() function
 fetch('https://jsonplaceholder.typicode.com/todos')
     .then((response)  => response.json())
     .then((json) => {
         let todos = json 
 
-        if(todos && todos.length > 0){
+        if(todos && todos.length > 0){ // Explain what this code does.
             todos.forEach(todo => {
                 generatedHTMLRows += `
                     <tr>
                         <td>${todo.id}</td>
                         <td>${todo.userId}</td>
                         <td>${todo.title}</td>
-                        <td>${todo.completed ? 'Completed':'Incompleted'}</td>
+                        <td>${todo.completed ? 'Completed':'Incomplete'}</td>
                     </tr>
-                `;
+                `; // Explain this block of code
             })
         }
 
