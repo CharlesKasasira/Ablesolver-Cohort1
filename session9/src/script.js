f10d7ef933bf325347a95dc961281d006a552af4
CharlesKasasira <charleskasasira01@gmail.com>
2021-08-05 09:17:04 +0300

    added todo form

--- session9/src/script.js
+++ session9/src/script.js
@@ -96,3 +96,67 @@ let retrieveWithPagination = (page = 1, numberOfItemsPerPage = 10) => {
 }
 
 retrieveWithPagination()
+
+
+const TODO_FORM = document.querySelector('form#new-todo')
+
+if(TODO_FORM != 'undefined'){
+  let title = ''
+  TODO_FORM.addEventListener('submit', event => {
+    event.preventDefault()
+    title =event.target[0].value;
+
+    if(!validate(title)){
+      return 
+    }
+
+    sendTodoToAPI(title)
+  })
+}
+
+let sendTodoToAPI = (title) => {
+  fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      userId: 1, 
+    }),
+    headers: {
+      "Content-type": 'application/json; charset=UTF-8',
+    },
+  })
+    .then(response => response.json())
+    .then(json => {
+      let { id } = json 
+      if(id) {
+        TODO_FORM.reset()
+        document.getElementById('todo-id').innerText = id 
+        document.getElementById('todo-alert').classList.remove('d-none')
+      }
+    })
+    .catch( error => console.log(error))
+};
+
+function validate(title){
+  return (title == 'undefined' || title.length < 3) ? false : true
+}
+
+(() => {
+  'use strict';
+  const FORMS = document.querySelectorAll('.needs-validation');
+
+  Array.prototype.slice.call(FORMS).forEach(
+    (FORM) => {
+      FORM.addEventListener(
+        'submit',
+        (event) => {
+          if(!FORM.checkValidity()){
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          FORM.classList.add('was-validated')
+        },
+        false
+      );
+    });
+})();
