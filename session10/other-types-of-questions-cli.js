727d7f9c89842a85d57788d3b3a4dbc634f61c0c
CharlesKasasira <charleskasasira01@gmail.com>
2021-08-17 11:04:42 +0300

    added day 4

---
+++ session10/other-types-of-questions-cli.js
@@ -0,0 +1,28 @@
+const INQUIRER = require('inquirer');
+
+const QUESTIONS = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'username'
+    },
+    {
+        type: 'password',
+        name: 'password',
+        message: 'Password'
+    }
+]
+
+const PASSWORD = '123456'
+
+INQUIRER.prompt(QUESTIONS).then((answers) => {
+    // console.log(`Hi ${answers['name']}!`);
+    let { name, password } = answers
+
+    if(password === PASSWORD){
+        console.log(`Hi ${name} you've successfully logged in`);
+    }
+    else{
+        console.log(`Hi ${name}, check your password`)
+    }
+})
