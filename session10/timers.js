8b384f8d7504ab2e2180c01672ea948953836c0d
CharlesKasasira <charleskasasira01@gmail.com>
2021-08-23 12:44:53 +0300

    setTimeout with parameter

--- session10/timers.js
+++ session10/timers.js
@@ -1,7 +1,15 @@
-setTimeout(() =>{
-    console.log('Run after 2 seconds')
-}, 2000);
+// setTimeout(() =>{
+//     console.log('Run after 2 seconds')
+// }, 2000);
 
-setTimeout(() => {
-    console.log('Ran after 50 milliseconds')
-}, 50);
+// setTimeout(() => {
+//     console.log('Ran after 50 milliseconds')
+// }, 50);
+
+// setTimeout with params 
+let greeting = (name, msg) => console.log(`${msg} ${name}`)
+
+setTimeout(greeting, 1000, 'David', 'Hola')
+setTimeout(greeting, 500, 'Abudi', 'Agandi')
+
+
