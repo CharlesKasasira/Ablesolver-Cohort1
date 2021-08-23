0d95631afb1c03d8ebc03e4e60dc5339f491a8e4
CharlesKasasira <charleskasasira01@gmail.com>
2021-08-23 12:54:06 +0300

    setTimeout ID usecase

--- session10/timers.js
+++ session10/timers.js
@@ -7,9 +7,37 @@
 // }, 50);
 
 // setTimeout with params 
-let greeting = (name, msg) => console.log(`${msg} ${name}`)
+// let greeting = (name, msg) => console.log(`${msg} ${name}`)
 
-setTimeout(greeting, 1000, 'David', 'Hola')
-setTimeout(greeting, 500, 'Abudi', 'Agandi')
+// setTimeout(greeting, 1000, 'David', 'Hola')
+// setTimeout(greeting, 500, 'Abudi', 'Agandi')
+
+// Addign setTimeout id to a variable for future use 
+// setTimeout ID usecase 
+
+const SET_TIME_OUT_ID = setTimeout(() => console.log('Hello Champions'), 5000)
+
+const INQUIRER = require('inquirer')
+
+const QUESTIONS = [
+    {
+        type: 'input',
+        name: 'number',
+        message: ' Enter any number',
+    }
+];
+
+INQUIRER.prompt(QUESTIONS).then((answers) => {
+    let  { number } = answers;
+    
+    if(number == 3){
+        clearTimeout(SET_TIME_OUT_ID)
+        console.log(`setTimeout wouldn't run`)
+    }
+    else
+    {
+        console.log(SET_TIME_OUT_ID)
+    }
+});
 
 
