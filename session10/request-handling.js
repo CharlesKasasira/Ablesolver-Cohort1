4f50d60cddc2110277a144b2f6beb4a2dafa65cf
CharlesKasasira <charleskasasira01@gmail.com>
2021-09-07 10:27:42 +0300

    making requests with node.js

---
+++ session10/request-handling.js
@@ -0,0 +1,18 @@
+
+const https = require('https')
+const options = {
+    hostname: 'example.com',
+    port: 443,
+    path: '/todos',
+    method: 'GET'
+}
+
+const request = https.request(options, (response) =>{
+    console.log(`statusCode ${response.statusCode}`)
+
+    response.on('data', returnedData => process.stdout.write(returnedData))
+})
+
+request.on('error', (error) => console.log(error))
+
+request.end()
