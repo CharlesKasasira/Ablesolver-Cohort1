b3813ac38980b16d4a0ba1cd32e7e6b719bf15ef
CharlesKasasira <charleskasasira01@gmail.com>
2021-09-15 09:51:59 +0300

    manage directories

---
+++ session10/managing-directories-2.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+const folderName = 'test'
+const folderName2 = "test2"
+
+try{
+    if(!fs.existsSync(folderName)){
+        fs.mkdirSync(folderName)
+        console.log('Folder created successfully!')
+    } else {
+        console.log(`Directory ${folderName} exists`)
+    }
+
+    if(!fs.existsSync(folderName2)){
+        fs.mkdirSync(folderName2)
+        console.log('Folder2 created successfully!')
+    } else {
+        console.log(`Directory ${folderName2} exists`)
+    }
+} catch (err){
+    console.log(err)
+}
