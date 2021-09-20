fb06e2da03dd896182353aadebaa1435c2e5001c
CharlesKasasira <charleskasasira01@gmail.com>
2021-09-20 11:16:07 +0300

    using buffer

--- session10/http-module.js
+++ session10/http-module.js
@@ -1,3 +1,7 @@
 const http = require('http')
+// Get information about the http module
+console.log(http);
 
-console.log(http.STATUS_CODES)
+console.log(http.STATUS_CODES) // all http status_codes and descriptions.
+
+console.log(http.METHODS) // all http methods
